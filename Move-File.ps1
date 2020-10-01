79c8d899524efed7f96351cb91c7bccbf479594b
Kugathasan Janarthanan <34956146+kujalk@users.noreply.github.com>
2020-10-01 15:30:55 +0800

    Version 2

--- Move-File.ps1
+++ Move-File.ps1
@@ -1,30 +1,73 @@
 <#
 Purpose - 
- - Move files from folder A to B
- - If files already there, will rename to <file name>-<date>.txt
+ - To Move files from folder A to B
+ - If file already there in destination, it will be renamed to <file name>-<date>.extension
+ - If file is not already there, it will be moved with same name
+ - In destination folder, it will create sub directories as it is in Source folder
+
+Examples
+    Move-File -source "C:\FolderA" -destination "C:\FolderB" -extension ".txt"
 
 Date - 9/9/2020
 Developer - K.Janarthanan
-Version - 1
+Version - 2
 #>
 
-$source_folder="E:\sample1"
-$destination_folder="E:\sample2"
+[CmdletBinding()]
+param(
+        [Parameter(Mandatory=$true)]
+        [string]$source,
 
-$source=Get-ChildItem -Path $source_folder -Recurse  | Where-Object {$_.Extension -eq '.txt'} | select Name -ExpandProperty Name
-$dest=Get-ChildItem -Path $destination_folder -Recurse | Where-Object {$_.Extension -eq '.txt'} | select Name -ExpandProperty Name
+        [Parameter(Mandatory=$true)]
+        [string]$destination,
 
-$date_name= (Get-date).tostring("yyyy-MM-dd_HH-mm-ss") 
+        [Parameter(Mandatory=$true)]
+        [string]$extension
+    )
 
-$source | ?{$dest -contains $_} | ForEach-Object {
-    $name=$_.split(".")[0]
-    $ext=$_.split(".")[1]
-    Write-Host "Already $_ file found in destination. Will be moved with new name $name-$date_name.$ext"
-    Move-Item -Path "$source_folder\$_" -Destination "$destination_folder\$name-$date_name.$ext"
+try    
+{
+    (Get-ChildItem -Path $source -Recurse -File | where-object {$_.extension -eq $extension }).FullName | ForEach-Object{
+
+        if($_ -eq $null)
+        {
+            Write-Host "No any matching files found"
+            exit
+        }
+        
+        $file_name=$_.split("\")[-1]
+        $folder_structure=$_.replace($source,"").replace($file_name,"")
+
+        $dest_folder=$destination+$folder_structure
+        $dest_file=$dest_folder+$file_name
+
+        if(Test-Path -Path $dest_folder)
+        {
+            if(Test-Path -Path $dest_file -PathType Leaf)
+            {
+                Write-Host "$dest_file File is already present in destination"
+                $date_name=(Get-Date).ToString("yyyy-MM-dd_HH-mm-ss")
+                $name=$file_name.split(".")[0]
+                Move-Item -Path $_ -Destination "$dest_folder\$name-$date_name$extension" -EA Stop
+                Write-Host "$dest_file File is moved to destination as $dest_folder\$name-$date_name$extension"
+            }
+            else
+            {
+                Move-Item -Path $_ -Destination $dest_file -EA Stop
+                Write-Host "$dest_file File is not already present in destination, therefore moved it"
+            }
+        }
+        else 
+        {
+            Write-Host "Destination Folder $dest_folder is not present. Therefore will create it"
+            New-Item -Path $dest_folder -Type Directory -EA Stop
+            Move-Item -Path $_ -Destination $dest_file -EA Stop
+            Write-Host "$dest_file File is not already present in destination, therefore moved it"
+        }
+    }
 }
 
-$source | ?{$dest -notcontains $_} | ForEach-Object {
-    Write-Host "Moving $_ file to destination folder"
-    Move-Item -Path "$source_folder\$_" -Destination "$destination_folder\$_"
+catch
+{
+    Write-Host "Error occured : $_"
 }
-
